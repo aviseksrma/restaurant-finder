424f9e5570a0631389c111cff1c9a0b00dc9126e
Abhishek Sharma <avisek@Abhisheks-MacBook-Air.local>
2023-09-27 22:52:28 +0530

    navbar added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Provider from "./provider";
+import HeaderNavBar from "@/components/HeaderNavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Provider>{children}</Provider>
+        <Provider>
+          <HeaderNavBar />
+          {children}
+        </Provider>
       </body>
     </html>
   );
