4a13aee9c90dc8b67ad7f2859ff795cfbc97bfb7
Abhishek Sharma <avisek@Abhisheks-MacBook-Air.local>
2023-09-27 22:17:39 +0530

    next-auth integrated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    console.log("session:", session);
+    if (!session?.user) {
+      router.push("/login");
+    }
+  }, [session]);
+
   return (
     <div>
       <h1>Hello</h1>
+      <button onClick={() => signOut()}>Sign Out</button>
     </div>
   );
 }
